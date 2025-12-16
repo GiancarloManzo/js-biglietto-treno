b6bf7856a2dbdce17779fb1730ffb7f43337ecf2
Giancarlo <cooper458.gm@gmail.com>
2025-12-16 15:18:58 +0100

    added prezzo finale

--- biglietto.js
+++ biglietto.js
@@ -1,21 +1,27 @@
 const km = prompt("quanti km vuoi percorrere?");
-const età = prompt("quanti anni hai?");
+const eta = prompt("quanti anni hai?");
 
 const kmNumber = parseInt(km);
-const etàNumber = parseInt(età);
+const etaNumber = parseInt(eta);
 console.log(kmNumber, typeof kmNumber);
-console.log(etàNumber, typeof etàNumber);
+console.log(etaNumber, typeof etaNumber);
 
 const prezzoPerKm = 0.21;
 const prezzoBase = kmNumber * prezzoPerKm;
 console.log("km:", kmNumber);
+console.log("eta", etaNumber);
+
 console.log("prezzoPerKm:", prezzoPerKm, typeof prezzoPerKm);
 console.log("prezzoBase:", prezzoBase, typeof prezzoBase);
 
 let sconto = 0;
 
-if (etàNumber < 18) {
+if (etaNumber < 18) {
   sconto = 0.2;
-} else if (etàNumber > 65) {
+} else if (etaNumber > 65) {
   sconto = 0.4;
 }
+const prezzoFinale = prezzoBase - prezzoBase * sconto;
+const prezzoFormattato = prezzoFinale.toFixed(2);
+
+console.log("il prezzo finale del biglietto è : $" + prezzoFormattato);
