6cdc54e1c19ce17a06b8afbbf54406e6c87a6fc1
Giancarlo <cooper458.gm@gmail.com>
2025-12-16 14:49:45 +0100

    added kmnumber and etànumber

--- biglietto.js
+++ biglietto.js
@@ -1,2 +1,7 @@
 const km = prompt("quanti km vuoi percorrere?");
 const età = prompt("quanti anni hai?");
+
+const kmNumber = parseInt(km);
+const etaNumber = parseInt(età);
+console.log(kmNumber, typeof kmNumber);
+console.log(etaNumber, typeof etaNumber);
